4c8e12cdd1a18c7eab06a000579830b5f8adabdf
rqphy <raphael.ferreira@hetic.net>
2022-09-09 16:57:00 +0200

    :sparkles: start home

--- src/Experience/Utils/Sizes.js
+++ src/Experience/Utils/Sizes.js
@@ -7,15 +7,15 @@ export default class Sizes extends EventEmitter
         super()
 
         // Setup
-        this.width = window.innerWidth
-        this.height = window.innerHeight
+        this.width = window.innerWidth * 0.4
+        this.height = window.innerHeight * 0.8
         this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
         // Resize event
         window.addEventListener('resize', () =>
         {
-            this.width = window.innerWidth
-            this.height = window.innerHeight
+            this.width = window.innerWidth * 0.4
+            this.height = window.innerHeight * 0.8
             this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
             this.trigger('resize')
