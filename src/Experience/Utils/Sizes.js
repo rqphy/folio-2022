b8132d2e3fa95c0d8cb156deee093bd22b78971a
rqphy <raphael.ferreira@hetic.net>
2022-09-14 14:30:37 +0200

    :iphone: home > hero > canvas > handle mobile

--- src/Experience/Utils/Sizes.js
+++ src/Experience/Utils/Sizes.js
@@ -7,18 +7,34 @@ export default class Sizes extends EventEmitter
         super()
 
         // Setup
-        this.width = window.innerWidth * 0.4
-        this.height = window.innerHeight * 0.8
+        this.setSize()
         this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
         // Resize event
         window.addEventListener('resize', () =>
         {
-            this.width = window.innerWidth * 0.4
-            this.height = window.innerHeight * 0.8
+            this.setSize()
             this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
             this.trigger('resize')
         })
+    }
+
+    setSize = () =>
+    {
+        let width = 0
+        let height = 0
+        if(window.innerWidth > 860)
+        {
+            width = window.innerWidth * 0.4
+            height = window.innerHeight * 0.8
+        } else
+        {
+            width = Math.min(window.innerWidth, window.innerHeight) * 0.8
+            height = Math.min(window.innerWidth, window.innerHeight) * 0.8
+        }
+
+        this.width = width
+        this.height = height
     }
 }
