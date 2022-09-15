ce88050d9cc86fe63d238fdcf9d0fa285289762f
rqphy <raphael.ferreira@hetic.net>
2022-09-15 15:20:25 +0200

    :dizzy: tweak projects animations

--- src/Experience/Scroll.js
+++ src/Experience/Scroll.js
@@ -7,41 +7,46 @@ export const addScrollAnimations = () =>
         {
             scrollY:
             {
-                scale:
+                // scale:
+                // [
+                //     ["elCenterY-500", "elCenterY-200"],
+                //     [0, 1],
+                // ],
+                translateY:
                 [
                     ["elCenterY-500", "elCenterY-200"],
-                    [0.5, 1],
+                    [75, 0],
                 ],
                 opacity:
                 [
                     ["elCenterY-500", "elCenterY-200"],
                     [0, 1],
-                ]
+                ],
             }
         }
     )
 
-    lax.addElements(
-        '.title', 
-        {
-            scrollY:
-            {
-                translateY:
-                [
-                    ["elCenterY-400", "elCenterY"],
-                    ['elHeight+20', 0],
-                ],
-                skewX:
-                [
-                    ["elCenterY-400", "elCenterY"],
-                    [-20, 0],
-                ],
-                skewY:
-                [
-                    ["elCenterY-400", "elCenterY"],
-                    [3, 0],
-                ],
-            }
-        }
-    )
+    // lax.addElements(
+    //     '.title', 
+    //     {
+    //         scrollY:
+    //         {
+    //             translateY:
+    //             [
+    //                 ["elCenterY-400", "elCenterY"],
+    //                 ['elHeight+20', 0],
+    //             ],
+    //             skewX:
+    //             [
+    //                 ["elCenterY-400", "elCenterY"],
+    //                 [-20, 0],
+    //             ],
+    //             skewY:
+    //             [
+    //                 ["elCenterY-400", "elCenterY"],
+    //                 [3, 0],
+    //             ],
+    //         }
+    //     }
+    // )
 }
