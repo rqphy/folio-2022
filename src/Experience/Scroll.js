4f9b0b479d22d4d33b77d2ec09d70c7f6c8c6a59
rqphy <raphael.ferreira@hetic.net>
2022-09-15 12:37:13 +0200

    :dizzy: add titles anims

--- src/Experience/Scroll.js
+++ src/Experience/Scroll.js
@@ -9,14 +9,38 @@ export const addScrollAnimations = () =>
             {
                 scale:
                 [
-                    ["elInY", "elCenterY-100"],
+                    ["elCenterY-500", "elCenterY-200"],
                     [0.5, 1],
                 ],
                 opacity:
                 [
-                    ["elInY", "elCenterY-100"],
+                    ["elCenterY-500", "elCenterY-200"],
                     [0, 1],
                 ]
+            }
+        }
+    )
+
+    lax.addElements(
+        '.title', 
+        {
+            scrollY:
+            {
+                translateY:
+                [
+                    ["elCenterY-400", "elCenterY"],
+                    ['elHeight+20', 0],
+                ],
+                skewX:
+                [
+                    ["elCenterY-400", "elCenterY"],
+                    [-20, 0],
+                ],
+                skewY:
+                [
+                    ["elCenterY-400", "elCenterY"],
+                    [3, 0],
+                ],
             }
         }
     )
