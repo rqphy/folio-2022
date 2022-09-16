6f79d5fe0c291924f202fe8e6c5e99dd508d41cf
rqphy <raphael.ferreira@hetic.net>
2022-09-16 16:30:19 +0200

    :technologist: readd js to home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,2 +1,19 @@
 import './index.scss'
 console.log('home')
+import Experience from '/src/Experience/Experience'
+import { addScrollAnimations } from '/src/Experience/Scroll'
+import lax from 'lax.js'
+
+const experience = new Experience(document.querySelector('canvas.hero__webgl'))
+
+window.onload = function () {
+    lax.init()
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function () {
+      return window.scrollY
+    })
+
+    // Add animation bindings to elements
+    addScrollAnimations()
+}
